3412723f4fac0c24e044b0270074827168bbe91f
xruanjian <xruanjian@users.noreply.github.com>
2018-10-10 18:15:04 +0800

    理解private/protected/default/public作用域

---
+++ object/修饰符的各个作用域理解.cs
@@ -0,0 +1,19 @@
+namespace 修饰符的各个作用域理解{
+{
+    /*
+     修饰符	    类内部	同一个包	子类	任何地方
+     private	Yes
+     default	Yes	    Yes
+     Protected	Yes	    Yes 	    Yes
+     public	    Yes	    Yes	        Yes	    Yes
+     对于class的权限修饰符只可以用public和default
+     a.Public类可以在任意地方被访问
+     b.default类只可以被同一个包内部的类访问
+     c.protected类只可以被同一个包内部的类和子类访问
+     d.private类只可以被内部的类访问
+     */
+}
+
+
+
+}
