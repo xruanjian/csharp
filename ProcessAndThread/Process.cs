0a84e8af0cc556badfa31365d1494aa2070d5128
xruanjian <xruanjian@users.noreply.github.com>
2018-10-25 09:10:14 +0800

    了解进程的创建，利用进程打开文件，杀死进程

--- ProcessAndThread/Process.cs
+++ ProcessAndThread/Process.cs
@@ -25,6 +25,16 @@ namespace 进程和线程
                 Console.WriteLine(item);
             }
 
+            
+            //通过一个进程打开指定文件
+            ProcessStartInfo psi=new ProcessStartInfo(@"c:\programfile\1.exe");
+            
+            //创建进程对象
+            Process p=new Process();
+            p.StartInfo=psi;
+            p.Start();
+            
+            
             Console.Read();
         }
     }
