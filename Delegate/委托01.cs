354c545c9ffcdbcffbee98074e8bd5a61f5259ba
YiChen <xruanjian@gmail.com>
2018-11-07 22:41:31 +0800

    了解委托概念，如何建立一个委托，并实现

---
+++ Delegate/委托01.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text;
+/*
+委托就是方法调用方法，即给一个方法传递另外一个方法
+条件:必须有相同的签名
+步骤:
+01: 声明委托，必须在namespace开头声明，不能声明到类里面
+02: public delegate 来声明一个委托，必须具有相同签名
+03，传递参数并执行
+*/
+namespace 了解委托{
+//一定是在命名空间下面声明委托，而不是在类内部
+   public delegate void Conv(string[] s);
+  
+public class Program{
+static void Main(string[] args){
+   string[] names={"angaLaBAby","XuEYichen","JAckie chan"};
+   
+   //使用委托调用转换大写
+   Conv cv1=new Conv(ConvertUpper);
+   cv1(names);
+   
+   //使用委托打印
+   Conv cv2=ChainStr;
+   cv2(names);
+   
+   Console.ReadKey();
+}
+
+//全部变大写
+private static void ConvertUpper(string[] s){
+   for(int i=0; i<s.Length; i++){
+   							s[i]=s[i].ToUpper();
+   }  
+}
+
+//全部变小写
+private static void ConvertLower(string[] s){
+   for(int i=0; i<s.Length; i++){
+   							s[i]=s[i].ToLower();
+   }
+}
+
+
+private static void ChainStr(string[] s){
+								string str=null;
+									for(int i=0; i<s.Length; i++){
+									   str+="#######";
+									   str+=s[i];
+									}
+									Console.WriteLine(str);
+}
+
+}
+}
